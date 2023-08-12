ce05ab31f72038929741be170511bc1986902cec
lienadochuko <erhisowomedanielking@gmail.com>
2023-08-12 23:25:51 +0100

    smeaker company

--- src/components/BodyDetails.jsx
+++ src/components/BodyDetails.jsx
@@ -5,12 +5,12 @@ import '../App.css';
 const BodyDetails = () => {
     return(
         <div className="bodyWrapper">
-            <p className="company">SNEAKER COMPANY</p>
-            <p className="edition">Fall Limited Edition Sneakers</p>
-            <p className="info">These low-profile sneakers 
+            <span className="company">SNEAKER COMPANY</span>
+            <span className="edition">Fall Limited Edition Sneakers</span>
+            <span className="info">These low-profile sneakers 
              are your perfect casual wear companion.
              Featuring a durable rubber outer sole,
-              they’ll withstand everything the weather can offer</p>
+              they’ll withstand everything the weather can offer</span>
         </div>
     )
 }
